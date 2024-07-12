60bbf9eb5a1eca2e666c37e3a9a9871f655bb0ca
Mohit Bhatt <mohitbha@dhcp-10-120-137-34.wifi.oracle.com>
2024-07-12 10:16:27 +0530

    Changes done

--- src/main/java/com/training/bms/AlstromAppApplication.java
+++ src/main/java/com/training/bms/AlstromAppApplication.java
@@ -8,7 +8,7 @@ public class AlstromAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlstromAppApplication.class, args);
-		System.out.println("Started");
+		System.out.println("Mohit has done the changes");
 	}
 
 }
